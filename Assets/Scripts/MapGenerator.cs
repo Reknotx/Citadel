88d8f6e2aa8ba5b35d6ae2291189b647e56db238
Reknotx <chaseoconnor3@comcast.net>
2021-09-02 15:29:14 -0700

    Changes

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,18 +1,40 @@
+/*
+ * Author: Chase O'Connor
+ * Date: 9/2/2021
+ * 
+ * Brief: This script spawns in the map at the start of the game
+ * and each time the player moves to another floor.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The map generator that spawns in the rooms in a 6x6 grid. 
+/// </summary>
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Temporary Variables
+    public GameObject basePrefab;
+
+    private int roomSize = 30;
+    public int gridSize = 6;
+    #endregion
+
+    public void Start()
     {
-        
+        SpawnMap();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnMap()
     {
-        
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                GameObject temp = Instantiate(basePrefab, new Vector3(roomSize * j, roomSize * i), Quaternion.identity);
+            }
+        }
     }
 }
