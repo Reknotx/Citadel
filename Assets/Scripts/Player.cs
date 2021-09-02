cd3839e83dff8a6b80069b94b0b85e0bfffda5a5
HunterLE <HunterLawrencee@gmail.com>
2021-09-02 02:03:28 -0700

    Created basic player movement with input system package

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : Unit
 {
+
+    #region Player Stats
+    ///<summary>This is the players Input system.</summary>
+    private PlayerInputActions playerInputActions;
+
+    ///<summary>This is the range of detection to the ground.</summary>
+    public float Reach = 1f;
+    ///<summary>This tracks what the ground detection raycast hits.</summary>
+    RaycastHit hit;
+
+    ///<summary>This is the players actual health.</summary>
     public override int Health 
     { 
         get => base.Health; 
         set => base.Health = value; 
     }
+    #endregion
+
+
+    private void Awake()
+    {
+        #region Player Movement
+        ///<summary>The following is used to track player inputs and controls.</summary>
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.PlayerControl.Enable();
+        playerInputActions.PlayerControl.Jump.performed += Jump;
+        playerInputActions.PlayerControl.Movement.performed += movement;
+        playerInputActions.PlayerControl.Drop.performed += Drop;
+        #endregion
+    }
 
 
     public override void Update()
     {
+      
         base.Update();
+
+        #region Player Movement
+        ///<summary>This moves the player constantly while the input is held.</summary>
+        Vector2 inputVector = playerInputActions.PlayerControl.Movement.ReadValue<Vector2>();
+        _rigidBody.AddForce(new Vector3(inputVector.x, 0, 0) * speed, ForceMode.Force);
+        #endregion
+
+
+        #region Ground/Platform detection
+        ///<summary>This determines whether the unit is on a platform or not.</summary>
+        var groundCheck = transform.TransformDirection(Vector3.down);
+        Debug.DrawRay(transform.position, groundCheck * Reach, Color.red);
+        if (Physics.Raycast(transform.position, groundCheck, out hit, Reach) && hit.transform.tag == "platform")
+        {
+            onPlatform = true;
+            
+        }
+        else
+        {
+            onPlatform = false;
+           
+        }
+
+        ///<summary>This determines whether the unit is on the ground or not.</summary>
+        Debug.DrawRay(transform.position, groundCheck * Reach, Color.red);
+        if (Physics.Raycast(transform.position, groundCheck, out hit, Reach) && hit.transform.tag == "ground")
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        #endregion
     }
+
 }
