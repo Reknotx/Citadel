268adc6cee7aba63042925da5c874bfab93a3f7b
Chase O'Connor <chaseoconnor3@comcast.net>
2021-09-20 20:41:32 -0700

    shop ui should be ready to go. Just needs spell added to ui

--- Assets/Scripts/ShopUI.cs
+++ Assets/Scripts/ShopUI.cs
@@ -15,6 +15,7 @@ namespace ShopSystem
         public Button speedUpButton;
         public Button manaUpButton;
         public Button spellPotencyUpButton;
+        public Button firewallSpellButton;
 
         List<Button> shopButtons;
 
@@ -35,6 +36,8 @@ namespace ShopSystem
             speedUpButton.onClick.AddListener(() => BuyStatIncrease(info.speedUpInfo.statToIncrease));
             manaUpButton.onClick.AddListener(() => BuyStatIncrease(info.manaUpInfo.statToIncrease));
             spellPotencyUpButton.onClick.AddListener(() => BuyStatIncrease(info.spellPotencyUpInfo.statToIncrease));
+
+            firewallSpellButton.onClick.AddListener(() => BuySpell());
         }
 
         private void OnEnable()
@@ -130,6 +133,11 @@ namespace ShopSystem
                 spellPotencyUpButton.interactable = false;
             else
                 spellPotencyUpButton.interactable = true;
+        }
+
+        public void BuySpell()
+        {
+            Player.Instance.fireWall_prefab = info.fireWall.spellPrefab;
         }
     }
 }
