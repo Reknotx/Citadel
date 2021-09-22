82117b04d02643c01980abcd7378fb19deddef0b
Chase O'Connor <chaseoconnor3@comcast.net>
2021-09-21 22:45:06 -0700

    changing grid size back

--- Assets/Scripts/PlayerGoldTrackerScript.cs
+++ Assets/Scripts/PlayerGoldTrackerScript.cs
@@ -24,8 +24,6 @@ public class PlayerGoldTrackerScript : MonoBehaviour
     public int startingNumCart;
     public int startingNumMiner = 2;
     
-
-
     public bool updated = false;
     public bool sceneChanged = false;
     public bool playerDead = false;
