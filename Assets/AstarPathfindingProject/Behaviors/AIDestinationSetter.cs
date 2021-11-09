54fdf537df856ab552079c0d18d5b2687ad26d61
Chase O'Connor <chaseoconnor3@comcast.net>
2021-11-09 14:34:40 -0800

    updated a star

--- Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -16,14 +16,7 @@ namespace Pathfinding {
 	public class AIDestinationSetter : VersionedMonoBehaviour {
 		/// <summary>The object that the AI should move to</summary>
 		public Transform target;
-		public Renderer m_renderer;
 		IAstarAI ai;
-
-		public void Start()
-		{
-			m_renderer = this.GetComponent<Renderer>();
-			target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-		}
 
 		void OnEnable () {
 			ai = GetComponent<IAstarAI>();
@@ -40,10 +33,7 @@ namespace Pathfinding {
 
 		/// <summary>Updates the AI's destination every frame</summary>
 		void Update () {
-			if (m_renderer.isVisible)
-			{
-				if (target != null && ai != null) ai.destination = target.position;
-			}
+			if (target != null && ai != null) ai.destination = target.position;
 		}
 	}
 }
