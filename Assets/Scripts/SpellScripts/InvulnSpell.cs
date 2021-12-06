144348052102dc204097a6c3239feeb406300fee
LE46 <HunterLawrencee@gmail.com>
2021-12-05 19:32:36 -0800

    invuln spell working

--- Assets/Scripts/SpellScripts/InvulnSpell.cs
+++ Assets/Scripts/SpellScripts/InvulnSpell.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvulnSpell : MonoBehaviour
+public class InvulnSpell : Spell
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public float invulnDurration = 5f;
+
+    public void Awake()
     {
-        
+
+        movingSpell = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        PlayerAnimationManager.Instance.ActivateTrigger("castPox");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        trackPlayer();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        TriggerSpell(player);
+        StartCoroutine(delayDestroy());
+    }
+
+    protected override void TriggerSpell(GameObject target)
+    {
+
+        target.GetComponent<NewPlayer>().StartCoroutine(player.GetComponent<NewPlayer>().IFrames(invulnDurration));
+    }
+
+    protected override void Move()
+    {
+        ///Activate the movement logic here
+        return;
+    }
+
+    public void trackPlayer()
+    {
+
+
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + .8f, player.transform.position.z);
+    }
+
+    public IEnumerator delayDestroy()
+    {
+        yield return new WaitForSeconds(invulnDurration);
+        Destroy(this.gameObject);
     }
 }
