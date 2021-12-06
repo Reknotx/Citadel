6aff7cba3157d71bafd8e896893afee9d15f560c
LE46 <HunterLawrencee@gmail.com>
2021-12-05 21:46:44 -0800

    resume rework

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -31,8 +31,8 @@ namespace Menu
 
         public void OnDisable()
         {
-            Time.timeScale = 1f;
-            NewPlayer.Instance.isPaused = false;
+
+            resumeGame();
         }
         
 
@@ -48,6 +48,13 @@ namespace Menu
         #else
             Application.Quit();
         #endif
+        }
+
+        void resumeGame()
+        {
+            var player = GameObject.FindGameObjectWithTag("Player").GetComponent<NewPlayer>();
+            player.isPaused = false;
+            Time.timeScale = 1f;
         }
 
     }
